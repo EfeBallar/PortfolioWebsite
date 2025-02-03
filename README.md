1c4dcff78fb0a93aaef7b3f08413f440ec1237cb
Efe Ballar <efeballar@gmail.com>
2025-02-03 23:03:21 +0300

    no more scss

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 This is Efe Ballar's personal portfolio website.
 
-
 I've used HTML, CSS and JavaScript to build this website.
-This website has Home Page, About Me Page, 
-My Projects Page and Contact Me Page.
-
+This website has Home Page, About Me Page, and My Projects Page.
 
 The website can be reached at efeballar.com
 
--- app.js
+++ app.js
@@ -3,69 +3,6 @@ const sectBtns=document.querySelectorAll('.controls');
 const sectBtn=document.querySelectorAll('.control');
 const allSections=document.querySelector('.main-content');
 
-function isEmail(email){
-	return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email );	
-}
-
-function validateForm() {
-    var nameInput = document.getElementById("Name");
-    var emailInput = document.getElementById("Email");
-    var msgInput = document.getElementById("Message");
-
-    if (nameInput.value.trim() === "") {
-        alert("Please write your name.")
-        nameInput.focus();
-        return false;
-    }
-
-    if (emailInput.value.trim() === "") {
-        alert("Please write your email.")
-        emailInput.focus();
-        return false;
-    }
-
-    if (msgInput.value.trim() === "") {
-        alert("Please write your message.")
-        msgInput.focus();
-        return false;
-    }
-
-    if (isEmail(emailInput.value.trim())==false){
-        alert("Please write a valid email.")
-        emailInput.focus();
-        return false;
-    }
-    SendMail();
-    return true;
-}
-
-function SendMail(){
-    (function() {
-        emailjs.init('2vE0h-UO3XWGge8wJ');
-    })();
-    var params={
-        from_name:document.getElementById("Name").value,
-        email_id:document.getElementById("Email").value,
-        message:document.getElementById("Message").value
-    }
-    emailjs.send("service_kz68ic8","template_1fakq9s",params).then(function(res){
-        alert("Email sent successfully!")
-        document.getElementById("Name").value="";
-        document.getElementById("Email").value="";
-        document.getElementById("Message").value="";
-    })
-}
-
-function updateDateTime() {
-    let time=document.getElementById("local_time")
-
-    setInterval(() => {
-        let d = new Date();
-        time.innerHTML =d.toLocaleString('en-GB', { timeZone: 'Europe/Istanbul' });
-    }, 1000)
-}
-updateDateTime();
-
 function PageTransitions(){
     //Button click active class
     for (let i=0 ; i<sectBtn.length;i++) {
