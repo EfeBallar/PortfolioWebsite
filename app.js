91bd3d0844e3d9d3a98e954f2dcc839abfb54d00
Efe Ballar <efeballar@gmail.com>
2025-02-03 19:32:44 +0300

    main theme will be light

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function PageTransitions(){
     const themeBtn=document.querySelector('.theme-btn');
     themeBtn.addEventListener('click',()=>{
         let element=document.body;
-        element.classList.toggle('light-mode')
+        element.classList.toggle('dark-mode')
     })
 }
 PageTransitions();
